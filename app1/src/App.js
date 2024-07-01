81ffafc4c9b63e70286d22e16085d4c986c79122
taksaito <taksaito@student.42tokyo.jp>
2024-07-01 05:14:36 +0000

    fix app1

--- app1/src/App.js
+++ app1/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>App 1</p>
+        <p>App 1 v2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
